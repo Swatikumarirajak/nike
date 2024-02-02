8efb0bb5c905ae9fb17643eb4964beae5833fc85
Swati <swatikumari3048@gmail.com>
2024-02-03 02:49:02 +0530

    first commit

---
+++ src/section/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import Services from "./Sevices"
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export{
+    Hero,
+    PopularProducts,
+    SpecialOffers,
+    SuperQuality,
+    Services,
+    CustomerReviews,
+    Subscribe,
+    Footer
+} 
